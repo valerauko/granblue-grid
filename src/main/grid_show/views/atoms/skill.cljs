e01311124d5cf2f461a860b1d159c99e438587c1
Vale <valerauko@gmail.com>
2024-09-14 22:07:06 +0900

    Add job panel

---
+++ src/main/grid_show/views/atoms/skill.cljs
@@ -0,0 +1,27 @@
+(ns grid-show.views.atoms.skill
+  (:require [shadow.css :refer [css]]))
+
+(def $skill
+  (css {:background-image "url('/skill_bg.png')"
+        :background-position "right"
+        :background-repeat "no-repeat"
+        :width "260px"
+        :height "50px"
+        :display "flex"
+        :align-items "center"
+        :padding "10px 15px"
+        :justify-content "center"}))
+
+(def $skill-name
+  (css {:overflow "hidden"
+        :text-wrap "nowrap"
+        :white-space "nowrap"
+        :text-overflow "ellipsis"}))
+
+(defn view
+  [name]
+  [:div
+   {:class [$skill]}
+   [:span
+    {:class [$skill-name]}
+    (str name)]])
---
+++ src/main/grid_show/views/job.cljs
@@ -0,0 +1,35 @@
+(ns grid-show.views.job
+  (:require [grid-show.state :as state]
+            [grid-show.views.atoms.skill :as skill]
+            [re-frame.core :as rf]
+            [shadow.css :refer [css]]))
+
+(def $wrap
+  (css {:position "relative"}))
+
+(def $image
+  (css {}))
+
+(def $details
+  (css {:position "absolute"
+        :right "0"
+        :top "0"
+        :padding "15px"}))
+
+(defn view
+  []
+  (let [job @(rf/subscribe [::state/job])]
+    (js/console.debug (clj->js (:skills job)))
+    [:div
+     {:class [$wrap]}
+     [:img
+      {:src (str "https://prd-game-a-granbluefantasy.akamaized.net/assets/img/sp/assets/leader/p/"
+                 (:image job)
+                 ".png")
+       :draggable false
+       :class [$image]}]
+     (into
+      [:div
+       {:class [$details]}]
+      (map #(fn [item] [skill/view item]))
+      (:skills job))]))
