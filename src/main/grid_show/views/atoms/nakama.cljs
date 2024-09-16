9898725c54bb89adbc60b6f36d540f4784fd515f
Vale <valerauko@gmail.com>
2024-09-16 21:04:33 +0900

    This is no rename but I'll take it

---
+++ src/main/grid_show/views/atoms/nakama.cljs
@@ -0,0 +1,16 @@
+(ns grid-show.views.atoms.nakama
+  (:require [grid-show.views.molecules.plusable :as plusable]))
+
+(defn view
+  [$class {:keys [image plus level]}]
+  [:div
+   {:class [$class]}
+   [plusable/view
+    plus
+    [:img
+     {:src (str "https://prd-game-a1-granbluefantasy.akamaized.net/assets/img/sp/assets/npc/quest/"
+                image
+                ".jpg")}]]
+   (when level
+     [:span
+      (str "Lv" level)])])
