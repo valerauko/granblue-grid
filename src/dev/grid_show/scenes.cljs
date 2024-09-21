ae0d86717f6e4edc249c5ce1941f2b70bc687949
Vale <valerauko@gmail.com>
2024-09-21 20:46:39 +0900

    CSS has to be configured

--- src/dev/grid_show/scenes.cljs
+++ src/dev/grid_show/scenes.cljs
@@ -5,6 +5,9 @@
             [grid-show.scenes.molecules.plusable]))
 
 (defonce ^:export app
-  (portfolio.ui/start!))
+  (portfolio.ui/start!
+   {:config
+    ;; it's not /css/ because :dev-http serves from ext/css already
+    {:css-paths ["/ui.css"]}}))
 
 (defn ^:export init [])
