6b39163d77918242aaa7eb3b37cdf2e508e6f7bf
Vale <valerauko@gmail.com>
2024-09-21 20:59:20 +0900

    Add some other variants

--- src/dev/grid_show/scenes/molecules/plusable.cljs
+++ src/dev/grid_show/scenes/molecules/plusable.cljs
@@ -2,7 +2,7 @@
   (:require [grid-show.views.molecules.plusable :as plusable]
             [portfolio.reagent-18 :refer-macros [defscene]]))
 
-(defscene ^:export plusable
+(defscene ^:export plusable-vertical
   [:div
    {:style {:width "min-content"}}
    [plusable/view
@@ -10,4 +10,24 @@
     [:div
      {:style {:width "60px"
               :height "120px"
+              :background "black"}}]]])
+
+(defscene ^:export plusable-grid
+  [:div
+   {:style {:width "min-content"}}
+   [plusable/view
+    69
+    [:div
+     {:style {:width "95px"
+              :height "55px"
+              :background "black"}}]]])
+
+(defscene ^:export plusable-large
+  [:div
+   {:style {:width "min-content"}}
+   [plusable/view
+    69
+    [:div
+     {:style {:width "105px"
+              :height "180px"
               :background "black"}}]]])
